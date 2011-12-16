073e96c60da528a0fb34750bef4b925572d65703
Cyril Gravelier <cyril@18ruedivona.eu>
2011-12-16 13:27:46 +0100

    Contact page

--- src/Divona/StandingsBundle/Controller/PageController.php
+++ src/Divona/StandingsBundle/Controller/PageController.php
@@ -2,6 +2,8 @@
 namespace Divona\StandingsBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Divona\StandingsBundle\Entity\Enquiry;
+use Divona\StandingsBundle\Form\EnquiryType;
 
 class PageController extends Controller
 {
@@ -14,4 +16,34 @@ class PageController extends Controller
     {
         return $this->render('DivonaStandingsBundle:Page:about.html.twig');
     }
+
+    public function contactAction()
+    {
+        $enquiry = new Enquiry();
+        $form = $this->createForm(new EnquiryType(), $enquiry);
+
+        $request = $this->getRequest();
+        if ($request->getMethod() == 'POST') {
+            $form->bindRequest($request);
+
+            if ($form->isValid()) {
+                $message = \Swift_Message::newInstance()
+                    ->setSubject($enquiry->getSubject())
+                    ->setFrom($enquiry->getEmail())
+                    ->setTo($this->container->getParameter('divona_standings.emails.contact_email'))
+                    ->setBody($this->renderView('DivonaStandingsBundle:Page:contactEmail.txt.twig', array('enquiry' => $enquiry)));
+                $this->get('mailer')->send($message);
+
+                $this->get('session')->setFlash('divona-notice', 'Your contact enquiry was successfully sent. Thank you!');
+                // Redirect - This is important to prevent users re-posting
+                // the form if they refresh the page
+                return $this->redirect($this->generateUrl('DivonaStandingsBundle_contact'));
+            }
+        }
+
+        return $this->render('DivonaStandingsBundle:Page:contact.html.twig', array(
+            'form' => $form->createView()
+        ));
+    }
+    
 }
