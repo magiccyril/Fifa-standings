b0ad1afd80cf86b2958c2e814e61bcd69645f3ed
Cyril Gravelier <cyril@18ruedivona.eu>
2011-12-21 11:25:46 +0100

    Revamp month template to a more generic standing template

--- src/Divona/StandingsBundle/Controller/StandingsController.php
+++ src/Divona/StandingsBundle/Controller/StandingsController.php
@@ -5,17 +5,21 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class StandingsController extends Controller
 {
-    public function showAction($display = 'month')
+    public function showAction($display = false)
     {
-        $em = $this->getDoctrine()->getEntityManager();
+        if (!$display)
+        {
+            return $this->redirect($this->generateUrl('DivonaStandingsBundle_standings_show_month'));
+        }
 
+        $em = $this->getDoctrine()->getEntityManager();
         $standing = $em->getRepository('DivonaStandingsBundle:Game')->getStanding($display);
 
         if (!$standing) {
             throw $this->createNotFoundException('Unable to generate this standing.');
         }
 
-        return $this->render('DivonaStandingsBundle:Standings:month.html.twig', array(
+        return $this->render('DivonaStandingsBundle:Standings:standing.html.twig', array(
             'standing' => $standing,
         ));
     }
